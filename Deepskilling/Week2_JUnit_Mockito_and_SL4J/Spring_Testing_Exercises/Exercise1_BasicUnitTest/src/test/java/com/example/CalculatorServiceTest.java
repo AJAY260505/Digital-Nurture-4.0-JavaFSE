235f1a2d016c17200091a167eb2ad8d8e660f468
AJAY J <120657267+AJAY260505@users.noreply.github.com>
2025-07-02 13:50:16 +0530

    Exercise 1: Basic Unit Test for a Service Method completed

---
+++ Deepskilling/Week2_JUnit_Mockito_and_SL4J/Spring_Testing_Exercises/Exercise1_BasicUnitTest/src/test/java/com/example/CalculatorServiceTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorServiceTest {
+
+    @Test
+    public void testAdd() {
+        CalculatorService calculatorService = new CalculatorService();
+        int result = calculatorService.add(2, 3);
+        assertEquals(5, result, "Addition of 2 + 3 should be 5");
+    }
+}

d38ebfa09b90906172f4de1dc47e192713085fc7
AJAY J <120657267+AJAY260505@users.noreply.github.com>
2025-07-21 11:53:40 +0530

    api gatway completed

--- Deepskilling/Week5_Microservices/Exercise2_Building_API_Gateway/api-gateway/src/main/java/com/cognizant/api_gateway/LogFilter.java
+++ Deepskilling/Week5_Microservices/Exercise2_Building_API_Gateway/api-gateway/src/main/java/com/cognizant/api_gateway/LogFilter.java
@@ -1,11 +1,11 @@
-package com.cts.gateway.filters;
-
+package com.cognizant.api_gateway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+
 import reactor.core.publisher.Mono;
 
 @Component

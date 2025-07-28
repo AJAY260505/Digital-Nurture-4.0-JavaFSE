84f34d4e55045595b99d35043a42f2be04cbe38e
AJAY J <120657267+AJAY260505@users.noreply.github.com>
2025-07-28 22:24:26 +0530

    Exercise 1,2,3 completed

---
+++ Deepskilling/Week6_React/Exercise1/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    open: true,
+    port: 3000,
+  },
+  mode: "development",
+};
